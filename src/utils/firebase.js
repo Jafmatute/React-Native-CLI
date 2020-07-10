82890f464a2a3f23fc79d7241dbfe31ac4a3e41c
Developer <developer@Mac-de-Developer.local>
2020-07-09 17:42:24 -0700

    configure firebase birthday app

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB3BVwLuGVmpfrHhHyddkx26kUFbeQIIfs',
+  authDomain: 'birthday-30195.firebaseapp.com',
+  databaseURL: 'https://birthday-30195.firebaseio.com',
+  projectId: 'birthday-30195',
+  storageBucket: 'birthday-30195.appspot.com',
+  messagingSenderId: '402006787652',
+  appId: '1:402006787652:web:7d58d19d08b954c6269e11',
+};
+
+export default firebase.initializeApp(firebaseConfig);
