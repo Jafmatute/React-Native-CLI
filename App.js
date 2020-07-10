a490a5f0f46ed26f44a3501a9840b1b3ba20795a
Developer <developer@Mac-de-Developer.local>
2020-07-09 17:58:19 -0700

    Checking if the user logged in to the application

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import firebase from './src/utils/firebase';
+import 'firebase/auth';
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      //console.log('Usuario Logged:', response);
+      setUser(response);
+    });
+  }, []);
+
+  if (user === undefined) return null;
   return (
     <SafeAreaView>
-      <Text>React native Cli</Text>
+      {user ? <Text>Logged</Text> : <Text>Not Logged</Text>}
     </SafeAreaView>
   );
 }
