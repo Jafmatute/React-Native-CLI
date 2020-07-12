943ecedda6f3080f7171450b62f9994a9567b1b4
Developer <developer@Mac-de-Developer.local>
2020-07-12 12:26:03 -0700

    Creating a horizontal scrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {StyleSheet, View, Image, Text, ScrollView} from 'react-native';
 
 //cuando se utliza ROW se alínea con alignItem:"center", sì es con COLUMN se utliza justifyContent
 //cuando flexDirection es ROW toma todo el ancho y cuando es column toma la altura usando FLEX:1
@@ -15,6 +15,38 @@ const App = () => {
           />
         </View>
       </View>
+
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}>¿Qué hacer en paris ?</Text>
+        <ScrollView horizontal={true}>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad1.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad2.jpg')}
+            />
+          </View>
+
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad3.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad4.jpg')}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </>
   );
 };
@@ -23,6 +55,19 @@ const styles = StyleSheet.create({
   banner: {
     height: 250,
     flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 250,
+    marginRight: 10,
   },
 });
 
