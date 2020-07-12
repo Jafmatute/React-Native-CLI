2e2682505826e94ae28f78900fa6353ed67b006a
Developer <developer@Mac-de-Developer.local>
2020-07-12 04:50:36 -0700

    aligning content with rows and columns

--- App.js
+++ App.js
@@ -18,26 +18,35 @@ const styles = StyleSheet.create({
   contenedor: {
     backgroundColor: 'cornflowerblue',
     flex: 1,
+    flexDirection: 'row',
+    //justifyContent: 'flex-end' alinear del lado derecho,
+    //justifyContent: 'flex-start', default
+    //justifyContent: 'center' alinea en el centro,
+    //justifyContent: 'space-between' generar un espacio entre todos,
+    //justifyContent: 'space-evenly' el mismos espacio que hay entre uno y otro es el mismo espacio que se va tener de izquierda a derecha,
+    justifyContent: 'space-around', //se distribuye con el mismo espacio al anterior de izquiera a derecha pero con espacio entre ambos,
+
+    //ALINEAR
+    //alignItems: 'flex-start' alinea arriba,
+    //alignItems: 'flex-end' alinea abajo,
+    //alignItems: 'strech' default,
+    alignItems: 'center',
   },
   box_one: {
     padding: 20,
     backgroundColor: 'blue',
-    flex: 1,
   },
   box_two: {
     padding: 20,
     backgroundColor: 'orange',
-    flex: 1,
   },
   box_tre: {
     padding: 20,
     backgroundColor: 'teal',
-    flex: 1,
   },
   box_four: {
     padding: 20,
     backgroundColor: 'navy',
-    flex: 2,
   },
 });
 
