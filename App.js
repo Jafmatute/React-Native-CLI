a322eb82acc746f85b1cf7528e1c69ecad8c4c8b
Developer <developer@Mac-de-Developer.local>
2020-07-12 12:01:02 -0700

    adding a banner image

--- App.js
+++ App.js
@@ -1,52 +1,28 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
+
+//cuando se utliza ROW se alínea con alignItem:"center", sì es con COLUMN se utliza justifyContent
+//cuando flexDirection es ROW toma todo el ancho y cuando es column toma la altura usando FLEX:1
 
 const App = () => {
   return (
     <>
-      <View style={styles.contenedor}>
-        <View style={styles.box_one}></View>
-        <View style={styles.box_two}></View>
-        <View style={styles.box_tre}></View>
-        <View style={styles.box_four}></View>
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            style={styles.banner}
+            source={require('./assets/img/bg.jpg')}
+          />
+        </View>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  contenedor: {
-    backgroundColor: 'cornflowerblue',
+  banner: {
+    height: 250,
     flex: 1,
-    flexDirection: 'row',
-    //justifyContent: 'flex-end' alinear del lado derecho,
-    //justifyContent: 'flex-start', default
-    //justifyContent: 'center' alinea en el centro,
-    //justifyContent: 'space-between' generar un espacio entre todos,
-    //justifyContent: 'space-evenly' el mismos espacio que hay entre uno y otro es el mismo espacio que se va tener de izquierda a derecha,
-    justifyContent: 'space-around', //se distribuye con el mismo espacio al anterior de izquiera a derecha pero con espacio entre ambos,
-
-    //ALINEAR
-    //alignItems: 'flex-start' alinea arriba,
-    //alignItems: 'flex-end' alinea abajo,
-    //alignItems: 'strech' default,
-    alignItems: 'center',
-  },
-  box_one: {
-    padding: 20,
-    backgroundColor: 'blue',
-  },
-  box_two: {
-    padding: 20,
-    backgroundColor: 'orange',
-  },
-  box_tre: {
-    padding: 20,
-    backgroundColor: 'teal',
-  },
-  box_four: {
-    padding: 20,
-    backgroundColor: 'navy',
   },
 });
 
