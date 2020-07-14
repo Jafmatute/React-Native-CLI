981f1f23024d53add203485695e1e44e6255c526
Developer <developer@Mac-de-Developer.local>
2020-07-14 14:13:26 -0700

    Creating form to create birthdays

--- src/components/AddBirthday.js
+++ src/components/AddBirthday.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function AddBirthday() {
   return (
-    <View>
-      <Text>add cumpleaños</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre"
+          placeholderTextColor="#969696"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Apellido"
+          placeholderTextColor="#969696"
+        />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    color: '#fff',
+    width: '80%',
+    marginBottom: 25,
+    backgroundColor: '#1e3040',
+    paddingHorizontal: 20,
+    //paddingRight: 50,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#1e3040',
+    borderRadius: 50,
+  },
+});
