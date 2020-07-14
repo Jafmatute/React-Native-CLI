540777b4d43e23d0e19763774c69354a2ce0d96b
Developer <developer@Mac-de-Developer.local>
2020-07-14 08:30:14 -0700

    Creating the login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import {validateEmail} from '../utils/validations';
+import firebase from '../utils/firebase';
 
 export default function LoginForm(props) {
   const {changeForm} = props;
   //console.log(changeForm);
+  const login = () => {
+    //console.warn('OK');
+  };
   return (
-    <View>
-      <Text>Login Form</Text>
+    <>
+      <TextInput
+        style={styles.input}
+        placeholder="Correo Electronico"
+        placeholderTextColor="#969696"
+      />
 
-      <TouchableOpacity onPress={changeForm}>
-        <Text style={styles.btnText}>Registrate</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        placeholderTextColor="#969696"
+        secureTextEntry={true}
+      />
+
+      <TouchableOpacity onPress={login}>
+        <Text style={styles.btnText}>Iniciar Sesiòn </Text>
       </TouchableOpacity>
-    </View>
+      <TouchableOpacity style={styles.btnRegistro} onPress={changeForm}>
+        <Text style={styles.btnText}>Registro</Text>
+      </TouchableOpacity>
+    </>
   );
 }
 
@@ -19,5 +44,22 @@ const styles = StyleSheet.create({
   btnText: {
     color: '#fff',
     fontSize: 18,
+  },
+  input: {
+    height: 50,
+    color: '#fff',
+    width: '80%',
+    marginBottom: 25,
+    backgroundColor: '#1e3648',
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    borderWidth: 1,
+    fontSize: 18,
+    borderColor: '#1e3648',
+  },
+  btnRegistro: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 20,
   },
 });
