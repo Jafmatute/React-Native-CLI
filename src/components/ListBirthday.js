9eb3c5de47637329fce3828ed4e32e0dc80fc8b8
Developer <developer@Mac-de-Developer.local>
2020-07-17 13:51:02 -0700

    Getting the birthday list by user from firebase

--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -1,10 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import ActionBar from './ActionBar';
 import AddBirthday from './AddBirthday';
+import firebase from '../utils/firebase';
+import 'firebase/firestore';
+
+firebase.firestore().settings({experimentalForceLongPolling: true});
+
+const db = firebase.firestore(firebase);
 export default function ListBirthday(props) {
   const [showList, setShowList] = useState(true);
   const {user} = props;
+  const [birthday, setBirthday] = useState([]);
+  //console.log('cumpleaños', birthday);
+  useEffect(() => {
+    setBirthday([]);
+    db.collection(user.uid)
+      .orderBy('dateBirth', 'asc')
+      .get()
+      .then((response) => {
+        const itemArray = [];
+        response.forEach((doc) => {
+          const data = doc.data();
+          data.id = doc.id;
+          itemArray.push(data);
+          //console.log('documento', doc.data());
+          //console.log(itemArray);
+        });
+        setBirthday(itemArray);
+        //console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <View style={styles.container}>
       {showList ? (
