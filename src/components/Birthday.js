b49114fd77f703505d562d220cee40ff5f27e1b0
Developer <developer@Mac-de-Developer.local>
2020-07-17 16:40:08 -0700

    Differentiating current, upcoming and past birthdays

--- src/components/Birthday.js
+++ src/components/Birthday.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function Birthday(props) {
   const {birthday} = props;
+  const pasat = birthday.days > 0 ? true : false;
+  //console.log(pasat);
   return (
-    <View>
-      <Text> {birthday.name} </Text>
-    </View>
+    <TouchableOpacity
+      style={[
+        styles.card,
+        pasat
+          ? styles.pasat
+          : birthday.days === 0
+          ? styles.actual
+          : styles.current,
+      ]}>
+      <Text>
+        {birthday.name} {birthday.lastname}
+      </Text>
+    </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 60,
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    margin: 10,
+    borderRadius: 15,
+  },
+  actual: {
+    backgroundColor: '#559204',
+  },
+  current: {
+    backgroundColor: '#1ae1f2',
+  },
+  pasat: {
+    backgroundColor: '#820000',
+  },
+});
