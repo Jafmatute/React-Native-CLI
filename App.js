5a26ae7a35730b8371967049200efa0b313600cb
Developer <developer@Mac-de-Developer.local>
2020-07-19 12:06:32 -0700

    Installing React Navigation 5

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Button, Avatar} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 export default function App() {
   console.log('debugg-Ui');
   return (
     <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Avatar.Text size={24} label="XD" />
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <Avatar.Text size={24} label="XD" />
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </SafeAreaView>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
