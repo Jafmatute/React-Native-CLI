a9074a4d5e7358a302cc3b70fdd32f3c883f1c61
Developer <developer@Mac-de-Developer.local>
2020-07-19 09:13:14 -0700

    Configuring Vectors/Icons n react native the IOS AND Android

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
-import {Provider as PaperProvider, Avatar} from 'react-native-paper';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {Button, Avatar} from 'react-native-paper';
 export default function App() {
   console.log('debugg-Ui');
   return (
     <PaperProvider>
       <SafeAreaView style={styles.container}>
         <Avatar.Text size={24} label="XD" />
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
       </SafeAreaView>
     </PaperProvider>
   );
